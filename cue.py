9eecfe53bc5d31df4391065f0d6f7c6fe7c0b4a4
Joseph Dwyer <dwyer.joe@gmail.com>
2012-05-14 21:02:01 -0500

    adding ability to have multiple global configuration files, specifically having each project in its own file in the global space. This also allows grouping of similar configuration settings like editors / source control providers which should make it eaiser on users to find modules and install the ones they want. Also, eliminating the use of global variables as much as possilbe, and reworking the register, unregister commands to use the new multi file system, and run_task to run find the task correctly.

--- cue.py
+++ cue.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import json
+import collections
 from subprocess import call
 
 parser = argparse.ArgumentParser(description="CUE!")
@@ -13,128 +14,161 @@ options.add_argument('-p', '--project',
 options.add_argument('task',
                     help="Specify the task")
 
+global_config_dir_path = os.path.join(os.getenv('HOME'), ".cue")
+extension = '.cueconf'
+
 args = None
-conf = None
-cuefile = None
 
 
-def save_conf():
-    f = open(os.path.join(os.getenv("HOME"), ".cueconf"), "w")
-    json.dump(conf, f, indent=4)
-    f.close()
+def get_global_conf():
 
+    if not os.path.exists(global_config_dir_path):
+        os.makedirs(global_config_dir_path)
 
-def get_cueconf():
-    cueconf_path = os.path.join(os.getenv('HOME'), ".cueconf")
-    if not os.path.isfile(cueconf_path):
-        print "~/.cueconf doesn't exist"
-        exit()
+    cueconf_file_paths = [os.path.join(global_config_dir_path, file) for file in os.listdir(global_config_dir_path) \
+                            if file.lower().endswith(extension)]
 
-    try:
-        cueconf = json.load(open(cueconf_path))
-    except:
-        print "~/.cueconf is not valid json"
-        exit()
-
-    if "projects" not in cueconf:
-        print "~/.cueconf is missing projects section"
-        exit()
-
-    if "tasks" not in cueconf:
-        print "~/.cueconf is missing tasks section"
-        exit()
-
-    return cueconf
-
-
-def get_cuefile():
-    if args["project"]:
-        if args["project"] not in conf["projects"]:
-            print "Project %s not found" % args["project"]
+    cues = {}
+    for cueconf_path in cueconf_file_paths:
+        if not os.path.isfile(cueconf_path):
+            print "cueconf doesn't exist " + cueconf_path
             exit()
 
         try:
-            cf = json.load(open(args["projects"][args["project"]]))
+            cueconf_contents = json.load(open(cueconf_path))
         except:
-            print "%s is not valid json" % args["projects"][args["project"]]
+            print "cueconf is not valid json"
+            exit()
+        cues = recursive_update(cues, cueconf_contents)
+
+    if "projects" not in cues:
+        print "cueconf is missing projects section"
+        exit()
+
+    if "tasks" not in cues:
+        print "cueconf is missing tasks section"
+        exit()
+
+    return cues
+
+
+def get_project_conf(global_conf, project_name=None):
+    cf = None
+    if project_name:
+        if project_name not in global_conf["projects"]:
+            print "Project %s not found" % project_name
+            exit()
+
+        try:
+            cf = json.load(open(global_conf["projects"][project_name]))
+        except:
+            print "%s is not valid json" % \
+                    global_conf["projects"][project_name]
             exit()
 
     else:
-        for slug in conf["projects"]:
-            cuefile_path = conf["projects"][slug]
-            if cuefile_path[:-8] in os.getcwd():
-                if not os.path.isfile(cuefile_path):
-                    print "Project found but %s appears to not exist" % cuefile_path
+        for slug in global_conf['projects']:
+            project_conf_path = global_conf["projects"][slug]
+            if os.path.dirname(project_conf_path) in os.getcwd():
+                if not os.path.isfile(project_conf_path):
+                    print "Project found but %s appears to not exist" % \
+                            project_conf_path
                     exit()
 
-                cf = json.load(open(cuefile_path))
+                cf = json.load(open(project_conf_path))
                 break
-        if cf:
-            if "root_path" not in cf:
-                print "cuefile missing 'root_path'"
-                exit()
+    if cf:
+        if "root_path" not in cf:
+            print "project_conf missing 'root_path'"
+            exit()
 
-            if "slug" not in cf:
-                print "cuefile missing 'slug'"
-                exit()
+        if "slug" not in cf:
+            print "project_conf missing 'slug'"
+            exit()
 
-            if "name" not in cf:
-                print "cuefile missing 'name'"
-                exit()
+        if "name" not in cf:
+            print "project_conf missing 'name'"
+            exit()
 
-            if "tasks" not in cf:
-                print "cuefile missing 'tasks'"
-                exit()
-            elif "workon" not in cf["tasks"]:
-                print "cuefile tasks mising 'workon' task"
-                exit()
+        if "tasks" not in cf:
+            print "project_conf missing 'tasks'"
+            exit()
+        elif "workon" not in cf["tasks"]:
+            print "project_conf tasks mising 'workon' task"
+            exit()
+    else:
+        print "project_conf not found"
+        exit()
+
+    return cf
+
+
+def recursive_update(d, u):
+    "Recursively updates a dictionary like object."
+    for k, v in u.iteritems():
+        if isinstance(v, collections.Mapping):
+            r = recursive_update(d.get(k, {}), v)
+            d[k] = r
+        elif k in d and isinstance(d[k], collections.Iterable) \
+                and isinstance(v, collections.Iterable):
+            d[k] += v
         else:
-            print "cuefile not found"
-            exit()
-
-        return cf
+            d[k] = v
+    return d
 
 
-def register():
+def register(global_conf):
     print "Register!"
-    if os.path.isfile(os.path.join(os.getcwd(), ".cuefile")):
-        cuefile = json.load(open(os.path.join(os.getcwd(), ".cuefile")))
-        if cuefile["slug"] in conf["projects"]:
-            print "project slug already exists on the system"
-            exit()
 
-        conf["projects"][cuefile["slug"]] = os.path.join(cuefile["root_path"], ".cuefile")
-        save_conf()
+    proj_cue_path = os.path.join(os.getcwd(), extension)
+
+    if os.path.isfile(proj_cue_path):
+        project_conf_dict = json.load(open(proj_cue_path))
+
+        name = project_conf_dict['name']
+        slug = project_conf_dict['slug']
+
     else:
         name = raw_input("Name of Project: ")
         slug = raw_input("Project Slug: ")
-        root = os.path.join(os.getcwd(), ".cuefile")
-
         tasks = {"workon": []}
 
-        conf["projects"][slug] = root
-        save_conf()
+        project_conf_dict = {"name": name,
+                        "slug": slug,
+                        "root_path": proj_cue_path,
+                        "tasks": tasks}
 
-        cuefile_dict = {"name": name, "slug": slug, "root_path": root, "tasks": tasks}
-
-        f = open(os.path.join(os.getcwd(), ".cuefile"), "w+")
-        json.dump(cuefile_dict, f, indent=4)
+        f = open(proj_cue_path, "w+")
+        json.dump(project_conf_dict, f, indent=4)
         f.close()
 
-
-def unregister():
-    print "Unregister!"
-    if cuefile["slug"] not in conf["projects"]:
-        print "Project %s is not registered" % cuefile["slug"]
+    if slug in global_conf["projects"]:
+        print "project slug already exists on the system"
         exit()
 
-    del conf["projects"][cuefile["slug"]]
-    save_conf()
+    global_conf["projects"][slug] = proj_cue_path
+
+    f = open(os.path.join(global_config_dir_path, slug + extension), "w+")
+    json.dump({'projects': {slug: proj_cue_path}}, f, indent=4)
+    f.close()
+
+    return project_conf_dict
 
 
-def run_task(task_name):
+def unregister(global_conf, project_conf):
+    print "Unregister!"
+    if project_conf["slug"] not in global_conf["projects"]:
+        print "Project %s is not registered" % proj_conf["slug"]
+        exit()
+
+    del global_conf["projects"][proj_conf["slug"]]
+    os.remove(os.join(global_config_dir_path, name))
+
+
+def run_task(task_name, global_conf, project_conf):
     def exec_task(task):
-        if type(task) is dict:
+        print type(task)
+        if isinstance(task, collections.Mapping):
             if "exec" in task:
                 call(task["exec"])
                 if "onError" in task:
@@ -144,32 +178,30 @@ def run_task(task_name):
                 #respect it
                 pass
 
-            pass
-        elif type(task) is str:
+        elif isinstance(task, basestring):
             if task[0] == ':':
                 run_task(task[1:])
             else:
+                print 'call ' + task
                 call(task)
 
     task = None
 
     #Local
-    if task_name in cuefile["tasks"]:
-        task = cuefile["tasks"][task_name]
+    if task_name in project_conf["tasks"]:
+        task = project_conf["tasks"][task_name]
 
     #Global
     if not task:
-        if task_name in conf["tasks"]["global"]:
-            task = conf["tasks"]["global"][task_name]
+        if 'global' in global_conf['tasks'] and task_name in global_conf["tasks"]["global"]:
+            task = global_conf["tasks"]["global"][task_name]
 
     #By Group
     if not task:
-        for group in conf["tasks"]:
-            if group in cuefile:
-                for _type in group:
-                    if task_name in _type:
-                        task = _type[task_name]
-                        break
+        for group in global_conf["tasks"]:
+            if group in project_conf:
+                task = \
+                global_conf['tasks'][group][project_conf[group]][task_name]
             if task:
                 break
 
@@ -183,13 +215,13 @@ def run_task(task_name):
 if __name__ == '__main__':
     args = vars(parser.parse_args())
 
-    conf = get_cueconf()
+    global_conf = get_global_conf()
 
     if args["task"] == "register":
-        register()
+        project_conf = register(global_conf)
     elif args["task"] == "unregister":
-        cuefile = get_cuefile()
-        unregister()
+        project_conf = get_project_conf(global_conf, args['project'])
+        unregister(global_conf, project_conf)
     else:
-        cuefile = get_cuefile()
-        run_task(args["task"])
+        project_conf = get_project_conf(global_conf, args['project'])
+        run_task(args["task"], global_conf, project_conf)
